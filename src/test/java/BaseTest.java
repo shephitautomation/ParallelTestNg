71f5e55a055bad0a319af338a23915d13204c052
shephitautomation <shephali.automationlabs@gmail.com>
2020-07-23 14:10:06 +0530

    23

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,44 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+    //Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
+//    protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public static WebDriver driver;
+    public CapabilityFactory capabilityFactory = new CapabilityFactory();
+
+    @BeforeMethod
+    @Parameters(value={"browser"})
+    public void setup (String browser) throws MalformedURLException {
+        //Set Browser to ThreadLocalMap
+//        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilityFactory.getCapabilities(browser)));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+//    public WebDriver getDriver() {
+//        //Get driver from ThreadLocalMap
+//        return driver.get();
+//    }
+
+//    @AfterMethod
+//    public void tearDown() {
+//        getDriver().quit();
+//    }
+
+    @AfterClass void terminate () {
+        //Remove the ThreadLocalMap element
+        driver.close();
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,46 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ONUR on 03.12.2016.
+ */
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void GOOGLE1() {
+        System.out.println("Google1 Test Started! " + "Thread Id: " +  Thread.currentThread().getId());
+        driver.navigate().to("http://www.google.com");
+        System.out.println("Google1 Test's Page title is: " + driver.getTitle() +" " + "Thread Id: " +  Thread.currentThread().getId());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        System.out.println("Google1 Test Ended! " + "Thread Id: " +  Thread.currentThread().getId());
+    }
+
+    @Test
+    public void GOOGLE2() {
+        System.out.println("Google2 Test Started! " + "Thread Id: " +  Thread.currentThread().getId());
+        driver.navigate().to("http://www.google.com");
+        System.out.println("Google2 Test's Page title is: " + driver.getTitle() +" " + "Thread Id: " +  Thread.currentThread().getId());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        System.out.println("Google2 Test Ended! " + "Thread Id: " +  Thread.currentThread().getId());
+    }
+
+    @Test
+    public void GOOGLE3() {
+        System.out.println("Google3 Test Started! " + "Thread Id: " +  Thread.currentThread().getId());
+        driver.navigate().to("http://www.google.com");
+        System.out.println("Google3 Test's Page title is: " + driver.getTitle() +" " + "Thread Id: " +  Thread.currentThread().getId());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        System.out.println("Google3 Test Ended! " + "Thread Id: " +  Thread.currentThread().getId());
+    }
+
+    @Test
+    public void GOOGLE5() {
+        System.out.println("Google5 Test Started! " + "Thread Id: " +  Thread.currentThread().getId());
+        driver.navigate().to("http://www.google.com");
+        System.out.println("Google5 Test's Page title is: " + driver.getTitle() +" " + "Thread Id: " +  Thread.currentThread().getId());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        System.out.println("Google5 Test Ended! " + "Thread Id: " +  Thread.currentThread().getId());
+    }
+
+
+}
