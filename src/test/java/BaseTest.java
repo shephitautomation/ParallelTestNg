68052b7cbef26c3700c0e486d661f9de049782d6
shephitautomation <68685341+shephitautomation@users.noreply.github.com>
2024-05-24 07:42:25 +0530

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,5 @@
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
